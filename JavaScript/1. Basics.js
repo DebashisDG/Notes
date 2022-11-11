626e94707d625e4a3a8f5767d89fd893cbc2a233
Debashis <96524815+DebashisDG@users.noreply.github.com>
2022-11-11 07:22:15 +0530

    Add files via upload

---
+++ JavaScript/1. Basics.js
@@ -0,0 +1,80 @@
+/* 1. Varibles */
+/*
+let name = 'Debashis'; 
+console.log(name);
+
+// Vaiable names cannot be a reserved keyword.
+// They should be meaningful
+// They cannot start with a number
+// They cannot contain a space or hypen
+// USe camelCase notation
+// They are case-sensitive
+*/
+
+
+/* 2. Constatnts */
+// const interestRate = 0.3;
+
+
+/* 3. Primitive types */
+/*
+let name = 'Debashis'; //string literal
+let age = 30; //number literal 
+let isApproved = true; //boolean literal
+let fName = undefined;
+let lName = null;
+*/
+
+
+/* 4. Dynamic typing */
+// Type of a variable can be changed in run time.
+/*
+let name = 'Debashis';
+console.log(typeof name); 
+name = 24;
+console.log(typeof name);
+*/
+
+
+/* 5. Objects */
+/*
+const person = {
+	name: 'Debashis',
+	age: 23
+};
+person.name = 'Mike';
+console.log(person.name);
+person['name'] = 'John';
+console.log(person['name']);
+*/
+
+
+/* 6. Arrays */
+/*
+let colors = ['Blue', 'Red'];
+colors[2] = 'Green';
+console.log(colors[1]);
+console.log(colors.length);
+*/
+
+
+/* 7. Functions */
+/*
+function greet(name){ //parameter
+	console.log('Hello ' + name);
+}
+greet('Debashis'); //arguement
+greet('John');
+*/
+
+
+/* 8. Types of functions */
+/*
+function greet(name){
+	console.log('Hello ' + name);
+}
+function square(number){
+	return square;
+}
+let result = square(2);
+*/
