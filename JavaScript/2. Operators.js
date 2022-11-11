04bc60549093f422aa26e72e46754b3a73622341
Debashis <96524815+DebashisDG@users.noreply.github.com>
2022-11-11 20:32:26 +0530

    Add files via upload

---
+++ JavaScript/2. Operators.js
@@ -0,0 +1,85 @@
+/* Arithmetic Operators */
+/*
+let x = 10;
+let y = 3;
+console.log(x + y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+console.log(x++);
+console.log(++x);
+console.log(x--);
+console.log(--x);
+*/
+
+
+/* Assignment Operators */
+/*
+let x = 10;
+x += 5;
+console.log(x);
+*/
+
+
+/* Comparision Operators */
+/*
+let x = 1;
+console.log(x > 0);
+console.log(x >= 0);
+console.log(x < 0);
+console.log(x <= 0);
+console.log(x === 0);
+console.log(x !== 0);
+*/
+
+
+/* Equality Operator */
+/*
+console.log(1 === '1'); // Strict equality
+console.log(1 == '1'); // Lose equality
+*/
+
+
+/* Ternary Operators */
+/*
+let points = 110;
+let type = points > 100 ? 'gold' : 'silver';
+console.log(type);
+*/
+
+
+/* Logical Operators with non-booleans */
+/*
+console.log(true && true);
+console.log(true && false);
+console.log(true || true);
+console.log(true || false);
+console.log(!true);
+*/
+
+
+/* Falsy Values */
+/*
+1. Undefined
+2. null
+3. 0
+4. false
+5. ''
+6. Nan
+*/
+
+
+/* Bitwise Operators */
+/*
+console.log(1 | 2);
+console.log(1 & 2);
+const readPermission = 4;
+const writePermission = 2;
+const executePermission = 1;
+let myPermission = 0;
+myPermission = myPermission | readPermission | writePermission;
+let msg = (myPermission & readPermission) ? 'Yes' : 'No';
+console.log(msg);
+*/
+
